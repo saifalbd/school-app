938719621a78931ee69245ccbb90e1fcc16c2881
saifalbd <saifurbd89@gmail.com>
2023-04-26 16:44:40 +0600

    fixSlider

--- resources/avisitor/js/app.js
+++ resources/avisitor/js/app.js
@@ -1,13 +1,42 @@
 import Splide from '@splidejs/splide';
 
+
+document.addEventListener( 'DOMContentLoaded', function() {
+  const fileBox = document.getElementById('fileBox');
+  let input = fileBox.querySelector('input.file-input');
+  let imageBox = fileBox.querySelector('.image-box');
+  let fileNameTag = fileBox.querySelector('.file-name');
+  input.addEventListener('change',(event)=>{
+    const target = event.target;
+    let files = target.files;
+    if(files.length){
+      let file = files[0];
+      fileNameTag.textContent = file.name;
+      let reader = new FileReader;
+      reader.onload = function(){
+        imageBox.style.display = 'block';
+        imageBox.querySelector('img').src = reader.result;
+
+      }
+      reader.readAsDataURL(file)
+
+
+    }else{
+      fileNameTag.textContent = '';
+      imageBox.style.display = 'none';
+    }
+  })
+})
+
+
  document.addEventListener( 'DOMContentLoaded', function() {
-//     var splide = new Splide( '.splide',{
-//       type   : 'loop',
-//       autoplay:true
-//     } ).mount();
-//     splide.on( 'active', function () {
+    var splide = new Splide( '.splide',{
+      type   : 'loop',
+      autoplay:true
+    } ).mount();
+    splide.on( 'active', function () {
      
-//     } );
+    } );
 
 
 
@@ -53,29 +82,4 @@ import Splide from '@splidejs/splide';
 
   } );
 
-  document.addEventListener( 'DOMContentLoaded', function() {
-    const fileBox = document.getElementById('fileBox');
-    let input = fileBox.querySelector('input.file-input');
-    let imageBox = fileBox.querySelector('.image-box');
-    let fileNameTag = fileBox.querySelector('.file-name');
-    input.addEventListener('change',(event)=>{
-      const target = event.target;
-      let files = target.files;
-      if(files.length){
-        let file = files[0];
-        fileNameTag.textContent = file.name;
-        let reader = new FileReader;
-        reader.onload = function(){
-          imageBox.style.display = 'block';
-          imageBox.querySelector('img').src = reader.result;
-
-        }
-        reader.readAsDataURL(file)
-
-
-      }else{
-        fileNameTag.textContent = '';
-        imageBox.style.display = 'none';
-      }
-    })
-  })
+  
